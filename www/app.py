f539ff034e759a9e96b295e666a0746515d3bd36
zhouruikun <915684080@qq.com>
2018-04-29 10:50:29 +0800

    加入参数

--- www/app.py
+++ www/app.py
@@ -3,16 +3,19 @@ import logging; logging.basicConfig(level=logging.INFO)
 import asyncio, os ,json ,time
 from datetime import datetime
 from aiohttp import web
+import sys
 
 def index(request):
       return web.Response(body=u'<h1>Awesome</h1>', content_type='text/html',charset='utf-8')
 
 @asyncio.coroutine
 def init( loop):
+    ip = sys.argv[1]
+    port = sys.argv[2]
     app = web.Application(loop = loop)
     app.router.add_route('GET','/',index)
-    srv =yield from loop.create_server(app.make_handler(), '127.0.0.1' ,9000)
-    logging.info('server started at http://127.0.0.1:9000...')
+    srv =yield from loop.create_server(app.make_handler(), ip ,port)
+    print('server started at http://%s:%s...' % (ip,port))
     return srv
 
 loop = asyncio.get_event_loop()
